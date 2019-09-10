d54808483ed3c28ad36c92db3ada111c2901a432
Bojan Peric <bojan.peric@ev-box.com>
2019-09-10 12:18:10 +0200

    'basics2' class added - POST request example

---
+++ src/basic/basics2.java
@@ -0,0 +1,46 @@
+package basic;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class basics2 {
+
+    @Test
+    public void postData() {
+        RestAssured.baseURI = "http://216.10.245.166/maps/api/place/add/xml?key=qaclick123";
+
+        given().
+                queryParam("key", "qaclick123").
+                body("{" +
+                        "\"location\": {" +
+                            "\"lat\": -33.8669710," +
+                            "\"lng\": 151.1958750" +
+                        "}," +
+                        "\"accuracy\": 50," +
+                        "\"name\": \"Google Shoes!\"," +
+                        "\"phone number\": \"(02) 9374 4000\"," +
+                        "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\"," +
+                        "\"types\": [\"shoe store\"]," +
+                        "\"website\": \"http://www.google.com.au/\"," +
+                        "\"language\": \"en-AU\"" +
+                "}").
+                    when().
+                    post("/maps/api/place/add/json").
+                    then().assertThat().statusCode(200).
+                    and().contentType(ContentType.JSON).
+                    and().body("status", equalTo("OK"));
+
+
+        // Create a place =response (place id)
+
+        // delete Place = (Request - Place id)
+
+
+    }
+
+
+}
